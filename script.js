5feb8b46a1b46839edd168f52206b869f5865b99
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-07-03 19:40:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+//your code here
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+event.preventDefault();
+const fontsize = document.querySelector("#fontsize").value;
+const fontcolor = document.querySelector("#fontcolor").value;
+document.documentElement.style.setProperty("--fontsize", ${fontsize}px);
+document.documentElement.style.setProperty("--fontcolor", ${fontcolor});
+document.cookie = `fontsize=${fontsize};max-age=31536000`;
+document.cookie = `fontcolor=${fontcolor};max-age=31536000`;
+});
+
+const cookieString = document.cookie;
+const cookies = cookieString.split(";");
+for (const cookie of cookies) {
+const [name, value] = cookie.split("=");
+if (name.trim() === "fontsize") {
+document.documentElement.style.setProperty("--fontsize", ${value}px);
+} else if (name.trim() === "fontcolor") {
+document.documentElement.style.setProperty("--fontcolor", ${value});
+}
+}
